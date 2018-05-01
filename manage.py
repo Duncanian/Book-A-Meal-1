1ac9de141c6f30ac1f31076906499c87e7a58561
unknown <lennykmutua@gmail.com>
2018-05-01 11:47:14 +0300

    Add migration script

---
+++ manage.py
@@ -0,0 +1,19 @@
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from models import db
+from app import app
+
+
+manager = Manager(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
+
+# $ python manage.py db init
+# $ python manage.py db migrate
+# $ python manage.py db upgrade
+# $ python manage.py db --help
