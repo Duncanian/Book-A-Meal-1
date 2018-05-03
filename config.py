98085fbd4248e10b9b5497ab7c7e8129dc0e4f9d
unknown <lennykmutua@gmail.com>
2018-05-03 16:50:03 +0300

    Add environment variables

--- config.py
+++ config.py
@@ -1,20 +1,25 @@
 """Contains various settings for each process of development
 """
+from os import getenv
 
 
 class Config(object):
     """Base class with all the constant config variables"""
     DEBUG = False
     TESTING = False
-    SECRET_KEY = "!@#_)&^%$$epic8^%%$#@#%^&*(&^&"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SECRET_KEY = getenv('SECRET_KEY')
 
 
 class TestingConfig(Config):
     """Contains additional config variables required during testing"""
     DEBUG = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = getenv('TESTING_DATABASE_URI')
+    
 
 
 class DevelopmentConfig(Config):
     """Contains additional config variables required during development"""
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = getenv('DEVELOPMENT_DATABASE_URI')
