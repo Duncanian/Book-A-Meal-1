698c926df3185ce351d98e496716b7da31a71840
unknown <lennykmutua@gmail.com>
2018-06-20 17:13:04 +0300

    Update render custom 404 error message page

--- doc_app.py
+++ doc_app.py
@@ -2,6 +2,7 @@
 """
 import os
 
+from flask import render_template
 from flasgger import Swagger
 
 from app import create_app
@@ -409,6 +410,9 @@ def hello_world():
     "test that flask app is running"
     return "To view the docs visit: https://book-a-meal-api.herokuapp.com/apidocs"
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
